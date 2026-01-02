7b53b7fce6b16d4a9dfecbaf0e1eba655658b96f
Prathvin Pinto <prathvinpinto01@gmail.com>
2026-01-02 23:23:25 +0530

    some confitteee

--- auction-rt-ui/src/components/Modal/Modal.jsx
+++ auction-rt-ui/src/components/Modal/Modal.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Confetti from "react-confetti";
 
-const Modal = ({ children, onClose, width = "max-w-sm" }) => {
+const Modal = ({ children, onClose, width = "max-w-sm", showConfetti }) => {
   return ReactDOM.createPortal(
     <div
-      className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 px-3 sm:px-6"
+      className="fixed inset-0 z-45 flex items-center justify-center bg-black/50 px-3 sm:px-6"
       onClick={onClose}
     >
+      {showConfetti && (
+        <div className="fixed inset-0 z-50 pointer-events-none">
+          <Confetti />
+        </div>
+      )}
       <div
         className={`bg-white p-4 rounded-md w-full ${width}`}
         onClick={(e) => e.stopPropagation()}
